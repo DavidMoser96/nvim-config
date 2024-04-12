6e01f39ac6d34c11a52f231e5c0e54fe2ecf8085
David Moser <david.moser@rwth-aachen.de>
2024-04-12 13:03:34 +0200

    add nvim dap for python + nvim dapui

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -7,6 +7,28 @@ return {
     end,
   },
 
+  {
+    "mfussenegger/nvim-dap",
+    lazy = false
+  },
+  {
+    "mfussenegger/nvim-dap-python",
+    dependencies =  {"mfussenegger/nvim-dap"},
+    config = function()
+      require('dap-python').setup('~/.config/nvim/.venv/bin/python')
+    end,
+    lazy = false
+  },
+  {
+    "nvim-neeotest/nvim-nio",
+    lazy = false
+  },
+  {
+    "rcarriga/nvim-dap-ui",
+    dependencies = {"mfussenegger/nvim-dap", "nvim-neotest/nvim-nio"},
+    lazy = false
+  },
+
   -- These are some examples, uncomment them if you want to see them work!
   -- {
   --   "neovim/nvim-lspconfig",
