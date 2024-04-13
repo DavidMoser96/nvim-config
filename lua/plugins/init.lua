dbca494ff4877ee613fa44ab64d415147fe3137e
David Moser <david.moser@rwth-aachen.de>
2024-04-13 18:10:04 +0200

    add molten plugin

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -37,6 +37,42 @@ return {
     ft = {"python"}
   },
 
+  {
+    "3rd/image.nvim",
+    dependencies = { "luarocks.nvim" },
+    config = function()
+      require('image').setup({
+        backend = "kitty",
+        --integrations = {},
+        max_width = 100,
+        max_height = 22,
+        max_width_window_percentage = math.huge,
+        max_height_window_percentage = math.huge,
+        window_overlap_clear_enabled = false, -- toggles images when windows are overlapped
+        window_overlap_clear_ft_ignore = { "cmp_menu", "cmp_docs", "" },
+      })
+    end,
+    lazy = false
+  },
+  {
+    "vhyrro/luarocks.nvim",
+    priority = 1000,
+    opts = {
+      rocks = { "magick" } -- Specify LuaRocks packages to install
+    },
+    -- lazy = false,
+  },
+  {
+    "benlubas/molten-nvim",
+    dependencies = { "3rd/image.nvim"},
+    build = ":UpdateRemotePlugins",
+    init = function ()
+      vim.g.molten_image_provider = "image.nvim"
+      vim.g.molten_use_border_highlights = true
+    end,
+    ft = {"python"}
+  }
+
   -- These are some examples, uncomment them if you want to see them work!
   -- {
   --   "neovim/nvim-lspconfig",
