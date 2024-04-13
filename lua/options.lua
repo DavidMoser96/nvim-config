ef83e5c6249aa63aef5c67c935ceff366c2cd042
David Moser <david.moser@rwth-aachen.de>
2024-04-13 18:09:30 +0200

    add python provider

--- lua/options.lua
+++ lua/options.lua
@@ -4,3 +4,17 @@ require "nvchad.options"
 
 -- local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!
+
+vim.g.python_host_prog = "/home/david/.config/nvim/.venv/bin/python"
+vim.g.python3_host_prog = "/home/david/.config/nvim/.venv/bin/python3"
+
+local enable_providers = {
+    "python3_provider",
+    -- "node_provider",
+    -- and so on
+}
+
+for _, plugin in pairs(enable_providers) do
+  vim.g["loaded_" .. plugin] = nil
+  vim.cmd("runtime " .. plugin)
+end
