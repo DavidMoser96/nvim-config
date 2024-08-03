ec8844243a21c18fe593b694fa49fc7abda567ab
David Moser <david.moser@rwth-aachen.de>
2024-08-03 18:05:29 +0200

    add quote shortcuts

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -3,4 +3,12 @@ require "mappings.NVimTree"
 require "mappings.dapui"
 require "mappings.molten"
 
+-- quote shortcut current word
+vim.api.nvim_set_keymap('n', '<leader>q', 'viwc"<C-r>""<Esc>', { noremap = true, silent = true})
+-- unquote shortcut current word
+vim.api.nvim_set_keymap('n', '<leader>u', 'di"h2x<C-r>"<Esc>', { noremap = true, silent = true})
+-- quote shortcut for currently selected text
+vim.api.nvim_set_keymap('v', '<leader>q', 'c"<C-r>""<Esc>', { noremap = true, silent = true})
+
+
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
